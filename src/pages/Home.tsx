a0346c79b506b04438f21a012bee9d0aa877e71e
yuds <yuds@linkconnection.co.kr>
2023-07-25 18:53:00 +0900

    홈 tsx 추가

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface IProps {}
+
+function Home(props: IProps) {
+  return (
+    <div style={{}}>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
